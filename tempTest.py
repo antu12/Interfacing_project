b765731b451e260ad131cc25a26aa96ca681e669
antu12 <Arshad>
2016-11-18 13:11:02 +0000

    pir tested and database for temp added

--- tempTest.py
+++ tempTest.py
@@ -1,8 +1,10 @@
 import time
+import mysql.connector
 
+conn=mysql.connector.connect(user='root', password='Nopassword01', host='52.165.29.136', database='autohome')
 try:
         while True:
-                tempfile = open("/sys/bus/w1/devices/28-000006dfa76c/w1_slave")
+                tempfile = open("/sys/bus/w1/devices/28-0000073ebc1c/w1_slave")
                 thetext = tempfile.read()
                 tempfile.close()
                 tempdata = thetext.split("\n")[1].split(" ")[9]
@@ -10,7 +12,17 @@ try:
                 temperature = temperature / 1000
                 print temperature
 
-                time.sleep(1)
+	
+		
+		str_query = "INSERT INTO temp values('"+str(temperature)+"')"
+		cursor=conn.cursor()
+		cursor.execute(str_query)
+		conn.commit()
+	
+                time.sleep(5)
 except KeyboardInterrupt:
         pass
+
+
+
 
